81126fe4aa76d78e7bd9914a47e113aec6124350
Noah <nallephu12@gmail.com>
2021-12-15 14:30:19 +0100

    more structure for FSTree class, needs implementation still

--- task3/FSTree.cpp
+++ task3/FSTree.cpp
@@ -1,1 +1,19 @@
 #include "FSTree.h"
+
+FSTree::FSTree()
+{
+    std::cout << "Creating FSTree..." << std::endl;
+}
+
+FSTree::~FSTree()
+{
+    std::cout << "Destroying FSTree..." << std::endl;
+}
+void FSTree::buildFileTree()
+{
+    std::cout << "Building tree from file..." << std::endl;
+}
+void FSTree::insert(dir_entry entry)
+{
+    // TODO
+}
--- task3/FSTree.h
+++ task3/FSTree.h
@@ -1,5 +1,7 @@
 #include <string>
 #include <vector>
+#include <iostream>
+#include <cstdint>
 
 #ifndef FSTREE_H
 #define FSTREE_H
@@ -11,15 +13,28 @@ struct dir_entry {
     uint8_t type; // directory (1) or file (0)
     uint8_t access_rights; // read (0x04), write (0x02), execute (0x01)
 };
+struct Node {
+    dir_entry entry;
+    std::vector<Node*> children;
+    Node(dir_entry val)
+    {
+        dir = val;
+    }
+};
 
 class FSTree
 {
 private:
+    Node* root_node = nullptr;
+    int depth = 0;
 
 
 public:
     FSTree();
     ~FSTree();
+    // build up the FSTree from data
+    void insert(dir_entry entry);
+    void buildFileTree();
 };
 
 #endif /* FSTREE_H */
