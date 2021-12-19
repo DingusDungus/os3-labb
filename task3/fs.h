850e01b9e1bfc4885fb21ac46505eeb6208c0491
Noah <nallephu12@gmail.com>
2021-12-19 14:17:14 +0100

    initializing the test of the dir_enrty struct for good measure

--- task3/fs.h
+++ task3/fs.h
@@ -27,10 +27,10 @@
 
 struct dir_entry {
     char file_name[56] = ""; // name of the file / sub-directory
-    uint32_t size; // size of the file in bytes
-    uint16_t first_blk; // index in the FAT for the first block of the file
-    uint8_t type; // directory (1) or file (0)
-    uint8_t access_rights; // read (0x04), write (0x02), execute (0x01)
+    uint32_t size = 0; // size of the file in bytes
+    uint16_t first_blk = 0; // index in the FAT for the first block of the file
+    uint8_t type = 0; // directory (1) or file (0)
+    uint8_t access_rights = 0; // read (0x04), write (0x02), execute (0x01)
 };
 
 struct treeNode
